74d9c9a4ba5f0d035776b76be65694621e13e669
Fujino N's <67798965+FujinoNs@users.noreply.github.com>
2020-09-26 08:50:10 +0700

    Add files via upload

---
+++ Login database/wd_mainapp.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Login_database
+{
+    /// <summary>
+    /// Interaction logic for wd_mainapp.xaml
+    /// </summary>
+    public partial class wd_mainapp : Window
+    {
+        public wd_mainapp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
