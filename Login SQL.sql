558c6006da49b401b685eeda5838de3076c09f57
Fujino N's <67798965+FujinoNs@users.noreply.github.com>
2020-09-26 08:48:05 +0700

    Add files via upload

---
+++ Login SQL.sql
@@ -0,0 +1,34 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               10.4.14-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for launcher
+CREATE DATABASE IF NOT EXISTS `launcher` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `launcher`;
+
+-- Dumping structure for table launcher.login
+CREATE TABLE IF NOT EXISTS `login` (
+  `username` text COLLATE utf8_unicode_ci NOT NULL,
+  `password` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table launcher.login: ~3 rows (approximately)
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+REPLACE INTO `login` (`username`, `password`) VALUES
+	('admin', '1234'),
+	('user', '5678');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ Login database/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows;
+
+namespace Login_database
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MySqlConnection connect = new MySqlConnection("host=localhost;user=root;database=launcher");
+        MySqlDataAdapter adapter;
+        DataTable table;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void openConnect()
+        {
+            try
+            {
+                if (connect.State == ConnectionState.Closed)
+                {
+                    connect.Open();
+                }
+            }
+            catch { }
+        }
+        public void closeConnect()
+        {
+            try
+            {
+                if (connect.State == ConnectionState.Open)
+                {
+                    connect.Close();
+                }
+            }
+            catch { }
+        }
+
+        public void datalogin(string query)
+        {
+            try
+            {
+                openConnect();
+                adapter = new MySqlDataAdapter(query, connect);
+                table = new DataTable();
+                adapter.Fill(table);
+                if (table.Rows.Count > 0)
+                {
+                    this.Hide();
+                    wd_mainapp _Main = new wd_mainapp();
+                    _Main.Show();
+                }
+                else
+                {
+                    MessageBox.Show("ไม่พบข้อมูลผู้ใช้!", "แจ้งเตือน!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                closeConnect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "แจ้งเตือน!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void btn_login_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string login = "SELECT * FROM launcher.login WHERE username ='" + txt_user.Text + "' AND password='" + txt_pass.Password + "'";
+                datalogin(login);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "แจ้งเตือน!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
